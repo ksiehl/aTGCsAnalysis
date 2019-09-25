11210b059a563146fd2d5e33ef62b2421071da65
Kevin Siehl <kevin.siehl@wayne.edu>
2019-09-25 23:56:38 +0200

    adding rename script

---
+++ Common/test/rename.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+mv WJets_HT-100To200_ele.root WJets_Ht100To200-ext0_ele.root
+mv WJets_HT-100To200-ext1_ele.root WJets_Ht100To200-ext1_ele.root
+mv WJets_HT-100To200-ext1_mu.root WJets_Ht100To200-ext1_mu.root
+mv WJets_HT-100To200-ext2_ele.root WJets_Ht100To200-ext2_ele.root
+mv WJets_HT-100To200-ext2_mu.root WJets_Ht100To200-ext2_mu.root
+mv WJets_HT-100To200_mu.root WJets_Ht100To200-ext0_mu.root
+mv WJets_HT-1200To2500_ele.root WJets_Ht1200To2500-ext0_ele.root
+mv WJets_HT-1200To2500-ext_ele.root WJets_Ht1200To2500-ext_ele.root
+mv WJets_HT-1200To2500-ext_mu.root WJets_Ht1200To2500-ext_mu.root
+mv WJets_HT-1200To2500_mu.root WJets_Ht1200To2500-ext0_mu.root
+mv WJets_HT-200To400_ele.root WJets_Ht200To400-ext0_ele.root
+mv WJets_HT-200To400-ext1_ele.root WJets_Ht200To400-ext1_ele.root
+mv WJets_HT-200To400-ext1_mu.root WJets_Ht200To400-ext1_mu.root
+mv WJets_HT-200To400-ext2_ele.root WJets_Ht200To400-ext2_ele.root
+mv WJets_HT-200To400-ext2_mu.root WJets_Ht200To400-ext2_mu.root
+mv WJets_HT-200To400_mu.root WJets_Ht200To400-ext0_mu.root
+mv WJets_HT-2500ToInf_ele.root WJets_Ht2500ToInf-ext0_ele.root
+mv WJets_HT-2500ToInf-ext_ele.root WJets_Ht2500ToInf-ext_ele.root
+mv WJets_HT-2500ToInf-ext_mu.root WJets_Ht2500ToInf-ext_mu.root
+mv WJets_HT-2500ToInf_mu.root WJets_Ht2500ToInf-ext0_mu.root
+mv WJets_HT-400To600_ele.root WJets_Ht400To600-ext0_ele.root
+mv WJets_HT-400To600-ext_ele.root WJets_Ht400To600-ext_ele.root
+mv WJets_HT-400To600-ext_mu.root WJets_Ht400To600-ext_mu.root
+mv WJets_HT-400To600_mu.root WJets_Ht400To600-ext0_mu.root
+mv WJets_HT-600To800_ele.root WJets_Ht600To800-ext0_ele.root
+mv WJets_HT-600To800-ext_ele.root WJets_Ht600To800-ext_ele.root
+mv WJets_HT-600To800-ext_mu.root WJets_Ht600To800-ext_mu.root
+mv WJets_HT-600To800_mu.root WJets_Ht600To800-ext0_mu.root
+mv WJets_HT-800To1200_ele.root WJets_Ht800To1200-ext0_ele.root
+mv WJets_HT-800To1200-ext_ele.root WJets_Ht800To1200-ext_ele.root
+mv WJets_HT-800To1200-ext_mu.root WJets_Ht800To1200-ext_mu.root
+mv WJets_HT-800To1200_mu.root WJets_Ht800To1200-ext0_mu.root
+
+mv WW-signal_MWW-600To800_ele.root WW-aTGC_MWW-600To800_ele.root 
+mv WW-signal_MWW-600To800_mu.root WW-aTGC_MWW-600To800_mu.root 
+mv WW-signal_MWW-800ToInf_ele.root WW-aTGC_MWW-800ToInf_ele.root 
+mv WW-signal_MWW-800ToInf_mu.root WW-aTGC_MWW-800ToInf_mu.root 
+mv WZ-signal_MWZ-600To800_ele.root WZ-aTGC_MWZ-600To800_ele.root 
+mv WZ-signal_MWZ-600To800_mu.root WZ-aTGC_MWZ-600To800_mu.root 
+mv WZ-signal_MWZ-800ToInf_ele.root WZ-aTGC_MWZ-800ToInf_ele.root 
+mv WZ-signal_MWZ-800ToInf_mu.root WZ-aTGC_MWZ-800ToInf_mu.root 
+
+mv ttbar-powheg_ele.root ttbar_ele.root
+mv ttbar-powheg_mu.root ttbar_mu.root
+
+mv WW_ele.root WW-ext0_ele.root
+mv WW_mu.root WW-ext0_mu.root
+
+mv SingleTop-t-channel-top_ele.root t-ch-top_ele.root
+mv SingleTop-t-channel-top_mu.root t-ch-top_mu.root
+mv SingleTop-t-channel-antitop_ele.root t-ch-antitop_ele.root
+mv SingleTop-t-channel-antitop_mu.root t-ch-antitop_mu.root
+mv SingleTop-tW-channel-top_ele.root tW-ch-top_ele.root
+mv SingleTop-tW-channel-top_mu.root tW-ch-top_mu.root
+mv SingleTop-tW-channel-antitop_ele.root tW-ch-antitop_ele.root
+mv SingleTop-tW-channel-antitop_mu.root tW-ch-antitop_mu.root
+mv SingleTop-s-channel_ele.root s-ch_ele.root
+mv SingleTop-s-channel_mu.root s-ch_mu.root
